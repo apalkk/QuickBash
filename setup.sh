76f708c5a32a45f3c4e94b4e669cc083379f0107
Aadi P <108011872+apalkk@users.noreply.github.com>
2022-11-15 16:57:36 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,61 @@
+echo Enter Your Name 
+read Name
+if [ "$Name" != $USER ]
+then 
+  echo "Your name isn't your username"
+  exit 0
+else
+  echo "Correct Answer"
+fi
+
+func (){
+Comd=""
+echo Commencing Operations .......
+sleep 1
+echo Ready
+echo "load > to download Homebrew, wget, ffmpeg, imagemagick, ohmyzsh, git, tree, nmap, and iterm2 - all the coding classics"
+echo "acs  > to download aldente, hot, yt-dlp and iina to truly accesorize your device. (Make sure you have HomeBrew installed)"
+echo "smv  > to download telegram, signal, and discord, the triad of hacker socials (Make sure you have HomeBrew installed)"
+read Comd
+if [ "$Comd" == "load" ]
+then 
+    install
+elif [ "$Comd" == "acs" ]
+then 
+    brewInstall
+elif [ "$Comd" == "smv" ]
+then 
+    smv
+elif [ "$Comd" == "setup" ]
+then 
+    setup
+fi
+}
+
+install (){
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install wget
+  brew install ffmpeg
+  brew install qbitorrent
+  brew install tree
+  brew install imagemagick
+  brew install git
+  brew install nmap
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  brew install iterm2
+  #brew install htop
+}
+brewInstall (){
+  brew install aldente
+  brew install hot
+  brew install yt-dlp
+  brew install iina
+}
+
+socials (){
+  brew install --cask telegram
+  brew install --cask signal
+  brew install --cask discord
+}
+
+func
