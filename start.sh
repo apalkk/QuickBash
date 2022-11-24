c6e2862ca91670c268bfc7e3008fcb7202e15c53
Aadi P <108011872+apalkk@users.noreply.github.com>
2022-11-24 15:11:31 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 usr=$(whoami)
 #echo "alias cSet='bash /Users/$usr/QuickBash/core.sh'"
-echo "#The three lines following this line and this line itself belongs to QuickBash" >> ~/.zshrc
+echo "#The four lines following this line and this line itself belongs to QuickBash" >> ~/.zshrc
 echo "alias qinfo='bash /Users/$usr/QuickBash/core.sh'" >> ~/.zshrc
 source ~/.zshrc
 echo "alias qlang='bash /Users/$usr/QuickBash/lang.sh'" >> ~/.zshrc
 source ~/.zshrc
 echo "alias qset='bash /Users/$usr/QuickBash/setup.sh'" >> ~/.zshrc
 source ~/.zshrc
+echo "alias qcrypt='bash /Users/$usr/QuickBash/crypt.sh'" >> ~/.zshrc
+source ~/.zshrc
+echo "#<<Quickbash aliases end<<" >> ~/.zshrc
