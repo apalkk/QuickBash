4f358a6182d39a3f9c02e9af53a7b635574b84d3
Aadi P <108011872+apalkk@users.noreply.github.com>
2022-11-25 12:56:09 -0500

    Update specialCommands.sh

--- specialCommands.sh
+++ specialCommands.sh
@@ -1,6 +1,6 @@
 if [ $1 = "1" ]
 then 
-  path = $(mdfind -onlyin . -name 'main.rs' | grep -A 1 /Users)
+  path=$(mdfind -onlyin . -name 'main.rs' | grep -A 1 /Users)
   cd $path
 else 
   echo "Wrong Input"
