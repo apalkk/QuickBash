a152cea87edfc7be63d81a7da054795fc83f9c4c
Aadi P <108011872+apalkk@users.noreply.github.com>
2022-11-28 03:01:09 -0500

    Update specialCommands.sh

--- specialCommands.sh
+++ specialCommands.sh
@@ -1,6 +1,6 @@
 if [ $1 = "m" ]
 then 
-  path=$(mdfind -onlyin . -name 'main.rs' | grep -A 1 /Users)
+  path=$(mdfind -onlyin . -name 'main' | grep -A 1 /Users)
   nvim $path
 elif [ $1 = "u" ]
   cd /Users/$(whoami)
