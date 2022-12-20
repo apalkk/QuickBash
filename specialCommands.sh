7ff78e15a57dd59f6440dd3f179a34a961ef328a
AP <108011872+apalkk@users.noreply.github.com>
2022-12-20 15:43:50 -0500

    Update specialCommands.sh

--- specialCommands.sh
+++ specialCommands.sh
@@ -1,8 +1,4 @@
 if [ $1 = "m" ]
 then 
-  nvim $(mdfind -onlyin . -name 'main' | grep -A 1 /Users)
-elif [ $1 = "u" ]
-  cd /Users/$(whoami)
-else 
-  echo "Wrong Input"
+    nvim $(mdfind -onlyin . -name 'main' | grep -A 1 /Users)
 fi
